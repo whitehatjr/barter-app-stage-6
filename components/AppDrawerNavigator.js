06a474edf173161d52a756f4331ccb541196cb0c
whitehatjr <rajeev@whitehatjr.com>
2020-04-15 18:52:55 +0530

    added MyBarters screen

---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,24 @@
+
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import { AppTabNavigator } from './AppTabNavigator'
+import CustomSidebarMenu  from './CustomSidebarMenu';
+import MyBartersScreen from '../screens/MyBartersScreen';
+import SettingScreen from '../screens/SettingScreen';
+
+export const AppDrawerNavigator = createDrawerNavigator({
+  Home : {
+    screen : AppTabNavigator
+    },
+  MyBarters:{
+      screen : MyBartersScreen,
+    },
+    Setting : {
+      screen : SettingScreen
+    }
+},
+  {
+    contentComponent:CustomSidebarMenu
+  },
+  {
+    initialRouteName : 'Home'
+  })
